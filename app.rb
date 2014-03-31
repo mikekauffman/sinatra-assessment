9ef187d165e6342a0b6e96a5402182edeac2551e
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-31 09:28:35 -0600

    user can create a product that displays on the homepage under Welcome. This shows that form posts successfully to homepage

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
+  PRODUCTS = []
+
   get '/' do
-    erb :index
+    erb :index, :locals => {:products => PRODUCTS}
   end
 
   get '/products/new' do
     erb :new_product
+  end
+
+  post '/' do
+    PRODUCTS << params[:new_product]
+    redirect '/'
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -5,12 +5,14 @@ require 'spec_helper'
 Capybara.app = App
 
 feature 'User manages a homepage' do
-  scenario 'User can enter a product into form and click create product' do
+  scenario 'User can create a product to display on the homepage' do
     visit '/'
     expect(page).to have_content 'Welcome'
     click_link 'Add a Product'
     fill_in 'new_product', :with => 'Applesauce'
     click_on 'Create Product'
+    expect(page).to have_content 'Applesauce'
+    expect(page).to have_content 'Welcome'
   end
 end
 
