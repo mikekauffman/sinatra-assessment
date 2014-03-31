b59d30103c07e614f206996b725a62c4bb997b78
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-31 09:16:07 -0600

    Homepage now includes a link to add a product. ERB page was created to allow for this

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -5,9 +5,10 @@ require 'spec_helper'
 Capybara.app = App
 
 feature 'User manages a homepage' do
-  scenario 'User sees Welcome on the homepage' do
+  scenario 'User sees Welcome on the homepage and clicks a link called Add a Product' do
     visit '/'
     expect(page).to have_content 'Welcome'
+    click_link 'Add a Product'
   end
 end
 
